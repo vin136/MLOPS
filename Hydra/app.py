8262233c86c9039ea657a3fbf0b4e2dc5ceff859
vin136 <vinnuvinay008@gmail.com>
2021-12-28 23:40:27 -0500

    hydra basic config setup

---
+++ Hydra/app.py
@@ -0,0 +1,15 @@
+import hydra
+from omegaconf import DictConfig, OmegaConf
+import os
+
+@hydra.main(config_path="config", config_name="config.yaml")
+def read_params(cfg : DictConfig) -> None:
+    #some hydra quirks
+    print(os.getcwd())
+    print(hydra.utils.get_original_cwd())
+    #read params
+    print(f"Reading params from the config.yaml")
+    print(f"batch size = {cfg.batch_size}")
+
+if __name__ == "__main__":
+    read_params()
